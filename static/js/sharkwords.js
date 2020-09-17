164a323cd603f1fa9dc6012c79d3b19f64518227
Alexandra Sanchez <alexlsanchez@gmail.com>
2020-09-17 20:09:06 +0000

    Completed functions. Noted errors in code.

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -15,8 +15,13 @@ const createDivsForChars = (word) => {
   for (const char of word) {
     $('#word-container').append(`<div class="letter-box ${char}"></div>`);
   }
-
 };
+
+  //   VJS -->
+  //   const div = document.createElement('div');
+  //   div.classList.add('letter-box');
+  //   div.classList.add(letter);
+  //   document.querySelector('#word-container').append(div);
 
 
 // Loop over each letter in `ALPHABET` and generate buttons.
@@ -25,8 +30,13 @@ const generateLetterButtons = () => {
   for (const char of ALPHABET) {
     $('#letter-buttons').append(`<button>${char}</button>`);
   }
-
 };
+
+    // VJS -->
+    // const btn = document.createElement('button');
+    // btn.innerHTML = char;
+    // document.querySelector('#letter-buttons').append(btn);
+
 
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -34,25 +44,31 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-
-  const button = $(buttonEl); //this is a jquery button now
-  $('button').attr('disabled', true);
+  const button = $(buttonEl); 
+  $('button').attr('disabled', true); //change from disabled to true
 };
+  
+  //VJS -->
+  // buttonEl.setAttribute('disabled', true);
+
 
 
 // Return `true` if `letter` is in the word.
 //
 const isLetterInWord = (letter) => {
-  return $(`div.${letter}`)[0] !== undefined;
-
+  return $(`div.${letter}`)[0] !== undefined; //checking if guessed letter returns tru  
 };
+  // VJS -->
+  // document.querySelector(`div.${letter}`) !== undefined;
 
 
 // Called when `letter` is in word. Update contents of divs with `letter`.
 //
 const handleCorrectGuess = (letter) => {
-  // Replace this with your code
+  $(`div.${letter}`).html(letter);  //if isLetterInWord is true --> runs this fcn
 };
+  // VJS -->    
+  // document.querySelector(`div.${letter}`).innerHTML = letter;
 
 
 // Called when `letter` is not in word.
@@ -61,15 +77,44 @@ const handleCorrectGuess = (letter) => {
 // message. Otherwise, increment `numWrong` and update the shark image.
 //
 const handleWrongGuess = () => {
-  // Replace this with your code
+  if (numWrong === 5) {
+    $('#play-again').css('display', '');  //Display? 
+
+  } else {
+      numWrong += 1 
+      ('#shark-img img').attr('src', `/static/images/guess${numWrong}.png`);
+  }
 };
+  // VJS -->
+  // document.querySelector('#play-again').style.display = '';
+  // document.querySelector('#shark-img img').setAttribute('src',`images/guess${numWrong}.png`);
+
+
 
 
 // Reset game state. Called before restarting the game.
 //
 const resetGame = () => {
-  // Replace this with your code
+  numWrong = 0;
+
+  ('#shark-img img').attr('src', `/static/images/guess0.png`);
+
+  $('#play-again').css('display', 'none'); 
+
+  for (const el of $('#word-container').children()) {
+    el.remove();
+  }
+  for (const el of $('#letter-buttons').children()) {
+    el.remove();
+  }
 };
+  // also could use this to remove children:
+  // $('section#word-container').empty();
+  // $('section#letter-buttons').empty();
+  // VJS -->
+  // document.querySelector('#shark-img img').setAttribute('src',`/static/images/guess0.png`);
+  // document.querySelector('#play-again').style.display = 'none';
+  // 
 
 
 // This is like if __name__ == '__main__' in Python
@@ -85,7 +130,7 @@ const resetGame = () => {
     const clickedBtn = evt.target;
     disableLetterButton(clickedBtn);
 
-    const letter = clickedBtn.html();
+    const letter = clickedBtn.html(); //throwing a TypeError here
 
     if (isLetterInWord(letter)) {
       handleCorrectGuess(letter);
